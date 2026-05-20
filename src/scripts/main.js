a59688668ce709eeb6aa23b2320299592917dd36
Gland Siahaan <glandjermanoblessedsiahaan@gmail.com>
2026-05-20 15:38:39 +0700

    Add ARCDEV brand logo and frontend foundation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,16 @@
-﻿import "./navigation.js";
+﻿import Alpine from "alpinejs";
+import Lenis from "lenis";
+
+import "./navigation.js";
 import "./animations.js";
 import "./sliders.js";
 import "./language.js";
 
-console.log("ARCDEV University website frontend initialized.");
+window.Alpine = Alpine;
+Alpine.start();
+
+const lenis = new Lenis({
+  autoRaf: true
+});
+
+console.log("ARCDEV University frontend initialized.");
